4bacd53b529a8f7ed8ed4e33512769fe8cf6f7d3
ivan_l <swankyyy1@gmail.com>
2024-03-03 11:33:57 +0300

    realized search field on mane page at top

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductsListView.as_view(), name="main_page_view"),
-    path('category/<slug:slug>', views.CategoryListView.as_view(), name="category_list")
+    path('category/<slug:slug>', views.CategoryListView.as_view(), name="category_list"),
+    path('search/', views.PageTitleSearchResultsView.as_view(), name="page_title_search")
     
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -5,6 +5,8 @@ from .models import Product, Category
 
 
 class ProductsListView(ListView):
+    """All products at the home page"""
+
     model = Product
     template_name = "products/product_list.html"
     context_object_name = "products"
@@ -14,6 +16,15 @@ class ProductsListView(ListView):
 
 
 class CategoryListView(View):
+    """Products by categories"""
     def get(self, request, slug, *args, **kwargs):
         context = Product.objects.filter(categories__url=slug, is_published=True)
         return render(request, "products/product_list.html", {'products': context})
+
+
+class PageTitleSearchResultsView(View):
+    def get(self, request):
+        query = request.GET.get("q")
+        products = Product.objects.filter(name__icontains=query)
+        return render(request, "products/product_list.html", {"products": products})
+
