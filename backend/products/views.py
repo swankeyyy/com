5fa6a421edc5ff30568a50d7edcaf2e88e6e376f
ivan_l <swankyyy1@gmail.com>
2024-03-07 13:20:51 +0300

    filter by tags and search field now in 1 view by 1 url

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,40 +8,30 @@ from .models import Product, Category, Brand, Tag
 class ProductsListView(View):
     """All products at the home page"""
 
-    def get(self, request):
+    def get(self, request, **kwargs):
+        query = request.GET.get("q")
+        tags = any(request.GET.getlist("brand") or request.GET.getlist("tag"))
         content = Product.objects.filter(is_published=True)
+        if query:
+            content = content.filter(name__icontains=query)
+        elif tags:
+            content = content.filter(
+                Q(brand__name__in=request.GET.getlist("brand")) |
+                Q(tag__name__in=request.GET.getlist("tag"))
+            )
+
         return render(request, "products/product_list.html", {'products': content})
 
 
 class CategoryListView(View):
     """Products by categories"""
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, **kwargs):
         slug = kwargs['slug']
         context = Product.objects.filter(categories__url=slug, is_published=True)
         return render(request, "products/product_list.html", {'products': context})
 
 
-class PageTitleSearchResultsView(View):
-    """Queryset of results searched from top searchfield on page"""
-
-    def get(self, request):
-        query = request.GET.get("q")
-        products = Product.objects.filter(name__icontains=query)
-        return render(request, "products/product_list.html", {"products": products})
-
-
-class FilteredProductsView(View):
-    """Filtered products by brand or tag or by tag and brand
-        html all in templatetags, by the Q and | it works like python "or"
-    """
-
-    def get(self, request):
-        content = Product.objects.filter(
-            Q(brand__name__in=request.GET.getlist("brand")) |
-            Q(tag__name__in=request.GET.getlist("tag"))
-        )
-        return render(request, "products/product_list.html", {"products": content})
 
 
 class SortedView(View):
@@ -67,7 +57,3 @@ class ProductDetailView(View):
         content = Product.objects.get(url=slug)
         last_products = Product.objects.filter(is_published=True)[0:2]
         return render(request, self.template_name, {'product': content, "last_products": last_products})
-
-
-
-
