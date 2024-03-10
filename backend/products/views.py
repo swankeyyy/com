933af1cb38ebe5b254d3d225968754193660216b
ivan_l <swankyyy1@gmail.com>
2024-03-10 18:23:08 +0300

    add comment to detail page

--- backend/products/views.py
+++ backend/products/views.py
@@ -65,6 +65,7 @@ class ProductDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['comments'] = self.get_object().comments.order_by('-id')
         context['form'] = self.form
         context['test'] = 'abc'
         return context
