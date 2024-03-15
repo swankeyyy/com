98487b9fa33277acd66bae49423897e207f0e9e6
ivan_l <swankyyy1@gmail.com>
2024-03-15 07:44:04 +0300

    completed comments without recursioon answers. It can be add, can be delete by user

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('addcomment/<int:pk>', views.AddComment.as_view(), name="add_comment"),
-
+    path('deletecomment/<int:pk>', views.DeleteComment.as_view(), name="delete_comment"),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -4,10 +4,14 @@ from products.models import Product
 from .forms import CommentForm
 from .models import Comment
 
+
 class AddComment(View):
+    """Добавляет комментарий к странице товара
+    в форме в урл прокидывается ид товара, пользователь берется из request,
+    text  из формы.
+    В планах реализовать создание комментария через form.save()"""
 
     def post(self, request, pk):
-
         form = CommentForm(request.POST)
         if form.is_valid():
             product = Product.objects.get(id=pk)
@@ -16,3 +20,13 @@ class AddComment(View):
             return redirect("product_detail_view", product.url)
 
 
+class DeleteComment(View):
+    """Удаления комментарий по ид комментария.
+    Разрешение стоит через проверку в шаблоне залогиненного пользователя по ид с владельцем комментария.
+    Удаляется через Ид коммента в маршруте"""
+
+    def get(self, request, **kwargs):
+        comment = Comment.objects.get(id=kwargs['pk'])
+        url = comment.product.url
+        comment.delete()
+        return redirect("product_detail_view", url)
